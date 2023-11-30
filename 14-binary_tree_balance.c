30be55ba5a8321f63439ca292abeadb2789f8f0a
binyam <btechan@gmail.com>
2023-11-29 16:12:05 -0800

    task 13-14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,21 +8,21 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left = 0, right = 0;
+	size_t left = 0, right = 0;
 
-    if (!tree)
-        return (0);
-    if (tree->left != NULL)
-    {
-        left = binary_tree_height(tree->left);
-    }
-    if (tree->right != NULL)
-    {
-        right = binary_tree_height(tree->right);
-    }
-    if (left >= right)
-        return (left + 1);
-    return (right + 1);
+	if (!tree)
+		return (0);
+	if (tree->left != NULL)
+	{
+		left = binary_tree_height(tree->left);
+	}
+	if (tree->right != NULL)
+	{
+		right = binary_tree_height(tree->right);
+	}
+	if (left >= right)
+		return (left + 1);
+	return (right + 1);
 }
 
 /**
@@ -33,11 +33,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left, right;
+	int left, right;
 
-    if (!tree)
-        return (0);
-    left = binary_tree_height(tree->left);
-    right = binary_tree_height(tree->right);
-    return (left - right);
+	if (!tree)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (left - right);
 }
