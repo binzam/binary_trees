624e4cd8cf27c9e09ebd5aef11eccd2f70bbbd63
binyam <btechan@gmail.com>
2023-11-29 17:12:41 -0800

    task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,23 +3,15 @@
 /**
  * binary_tree_sibling -  finds the sibling of a node.
  * @node: a pointer to the node to find the sibling.
- * Return: the sibling of the node,
- * NULL if node or the parent is NULL or node has no sibling
+ * Return: NULL if node or the parent is NULL or node has no sibling
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
-
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	if (parent->left == node)
-	{
-		return (parent->right);
-	}
-	else
-	{
-		return (parent->left);
-	}
-	return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+
+	return (node->parent->left);
 }
