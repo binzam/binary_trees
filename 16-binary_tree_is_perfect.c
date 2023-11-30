7a05b8abb84a27196e4bf1a3b5a2a94af809b26c
binyam <btechan@gmail.com>
2023-11-29 16:57:31 -0800

    task 15-16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,23 +10,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
 
-	left = 0;
-	right = 0;
-	if (!tree)
-		return (0);
-	if (tree->left != NULL)
-	{
-		left = binary_tree_height(tree->left) + 1;
-	}
-	if (tree->right != NULL)
-	{
-		right = binary_tree_height(tree->right) + 1;
-	}
-	if (left >= right)
-		return (left);
-	return (right);
-}
+	if (tree == NULL)
+		return (-1);
 
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	return (1 + (left > right ? left : right));
+}
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect.
  * @tree: a pointer to the root node of the tree to check.
@@ -37,11 +28,24 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	size_t left, right;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
+
 	if (left == right)
-		return (1);
+	{
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			return (1);
+		}
+		else if (tree->left != NULL && tree->right != NULL)
+		{
+			return (binary_tree_is_perfect(tree->left)
+					&& binary_tree_is_perfect(tree->right));
+		}
+	}
+
 	return (0);
 }
