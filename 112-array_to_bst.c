1b5df743f6f9c6c61fbbe8a70b7ad8701e1e6b67
binyam <btechan@gmail.com>
2023-11-30 16:03:33 -0800

    task 27

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -10,13 +10,10 @@
 bst_t *array_to_bst(int *array, size_t size)
 {
 	bst_t *root = NULL;
-	size_t i;
+	unsigned int i;
 
 	for (i = 0; i < size; i++)
-	{
-		if (bst_insert(&root, array[i]) == NULL)
-			return (NULL);
-	}
+		bst_insert(&root, array[i]);
 
 	return (root);
 }
